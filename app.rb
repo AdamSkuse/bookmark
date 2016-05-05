dc419c531011b56357e88ff7a6319900db3fead1
AdamSkuse <a.t.skuse@gmail.com>
2016-05-05 16:09:19 +0100

    Add tag view

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ class BookmarkManager < Sinatra::Base
 
   get '/links' do
   	@links = Link.all
-  	erb :links
+  	erb :'/links/index'
   end
 
   get '/links/new' do
-    erb :new_link
+    erb :'/links/new'
   end
 
   post '/links' do
@@ -25,6 +25,12 @@ class BookmarkManager < Sinatra::Base
     link.tags << tag
     link.save
     redirect('/links')
+  end
+
+  get '/tags/:name' do
+    tag = Tag.all(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'/links/index'
   end
 
   # start the server if ruby file executed directly
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -10,4 +10,13 @@ feature 'Viewing Links' do
     end
   end
 
+  scenario 'should show links filtered by tag' do
+    Link.create(url: 'http://google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
+    Link.create(url: 'http://yahoo.com', title: 'Yahoo', tags: [Tag.first_or_create(name: 'search')])
+    visit('/tags/search')
+    within('ul#links') do
+      expect(page).to have_content('Google')
+      expect(page).to have_content('Yahoo')
+    end
+  end
 end
